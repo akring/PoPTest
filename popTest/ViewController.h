936b6fbf1fdda026876247c7cb5681f69620a6ae
Lvjun <akring@163.com>
2015-04-17 18:09:19 +0800

    Initial Commit

---
+++ popTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  popTest
+//
+//  Created by Akring on 15/4/17.
+//  Copyright (c) 2015年 Akring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ popTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  popTest
+//
+//  Created by Akring on 15/4/17.
+//  Copyright (c) 2015年 Akring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
