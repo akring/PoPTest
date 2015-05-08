a1b5759d0cf9f6be4030ac5185e59ed572679601
Akring <akring@AkringdeiMac.local>
2015-05-08 14:03:04 +0800

    111

--- popTest/ViewController.m
+++ popTest/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <MMTweenAnimation.h>
+#import <CSNotificationView.h>
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIButton *button;
 
 @end
 
@@ -16,7 +19,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [CSNotificationView showInViewController:self
+                                       style:CSNotificationViewStyleSuccess
+                                     message:@"Great, it works."];
+}
+- (IBAction)clicked:(id)sender {
+    
+    MMTweenAnimation *anim = [MMTweenAnimation animation];
+    anim.functionType   = MMTweenFunctionBounce;
+    anim.easingType     = MMTweenEasingOut;
+    anim.duration       = 2.0f;
+    anim.fromValue      = 0;
+    anim.toValue        = 200;
+    anim.animationBlock = ^(double c,double d,double v,id target,MMTweenAnimation *animation)
+    {
+        //c: current time, from the beginning of animation
+        //d: duration, always bigger than c
+        //v: value, after the change at current time
+        
+        UIButton *t = (UIButton*)target;
+        
+        _button.center = CGPointMake(t.frame.origin.x, v);
+    };
+    
+    [_button pop_addAnimation:anim forKey:@"center.y"];
 }
 
 - (void)didReceiveMemoryWarning {
